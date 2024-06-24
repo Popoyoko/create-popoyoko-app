0031cb7e888264d1ee3d610ac306c977100c6037
Emerson452 <emerson@popoyoko.fr>
2024-06-24 13:23:11 +0200

    ✨ Create-popoyoko

---
+++ README.md
@@ -0,0 +1,36 @@
+# create-popoyoko
+### This repository is a Popoyoko-UI-Boilerplate layer that acts as the CLI
+
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+## This is the **CLI Repo**
+
+This repository is a layer of Popoyoko-UI-Boilerplate.
+
+
+📦 [Popoyoko-UI-Boilerplate](https://github.com/Popoyoko/Popoyoko-UI-Boilerplate) <= Here is the boilerplate of popoyoko
+
+
+
+📦 [popoyoko-ui **package branch**](https://github.com/Popoyoko/popoyoko-ui/tree/package) <= Here is the build of the component library
+
+🕹️ [Here is the **storybook**](https://popoyoko.github.io/popoyoko-ui/) <= Here's the Storybook for viewing components
+
+📦 [Popoyoko-UI-Token-Generator](https://github.com/Popoyoko/popoyoko-ui/tree/dev-package) <= Here is the token generator
+
+
+## Scaffolding Your First Popoyoko Project :
+
+npm :
+
+```bash
+npm create popoyoko
+```
+
+bun :
+
+```bash
+bun create popoyoko
+```
+
+Uses the Popoyoko-UI-Token-Generator to create your tokens 
---
+++ bin/create-popoyoko.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+const readline = require('readline');
+const path = require('path');
+const fs = require('fs');
+const copyTemplate = require('../lib/copy-template');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Please specify the project directory:', (projectName) => {
+    if (!projectName) {
+        projectName = 'Popoyoko-UI-Boilerplate';
+
+    }
+
+    const projectPath = path.resolve(process.cwd(), projectName);
+    if (fs.existsSync(projectPath)) {
+        console.error(`Directory ${projectName} already exists.`);
+        rl.close();
+        process.exit(1);
+    }
+
+    const templatePath = path.join(__dirname, '../Popoyoko-UI-Boilerplate');
+    if (!fs.existsSync(templatePath)) {
+        console.error('Le directory du template n\'existe pas :', templatePath);
+        rl.close();
+        process.exit(1);
+    }
+
+    fs.mkdirSync(projectPath);
+    copyTemplate(path.join(__dirname, '../Popoyoko-UI-Boilerplate'), projectPath);
+
+    console.log(`Project ${projectName} created successfully!`);
+    console.log(`To get started:`);
+    console.log(`  cd ${projectName}`);
+    console.log(`  npm install`);
+    console.log(`  npm run dev`);
+
+    rl.close();
+});
